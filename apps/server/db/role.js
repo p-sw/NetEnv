cdca23624384f357df08498c07c2ab377c2c692a
p-sw <shinwoo.park@psw.kr>
2024-09-14 12:38:55 +0900

    refactor(server): make role crud as class

--- apps/server/db/role.js
+++ apps/server/db/role.js
@@ -2,71 +2,94 @@
  * @typedef {import('sqlite3').Database} Database
  */
 /**
- * @typedef {{ name: string; }} Role
+ * @typedef {{ name: string; }} IRole
  */
 
-/**
- * Find role by name, and return data of the role.
- * If no role found, return null.
- *
- * @param {Database} db - sqlite3 Database instance
- * @param {string} name - name of the role
- * @returns {Promise<Role | null>}
- */
-export function getRoleByName(db, name) {
-  return new Promise((resolve) => {
-    db.get(`SELECT * FROM Roles WHERE name = ?`, [name], function (_, row) {
-      if (!row) resolve(null);
-      resolve(row);
+/* ======== Role CRUD ======== */
+
+class Role {
+  /**
+   * @type {Database}
+   */
+  db;
+  /**
+   * @type {IRole}
+   */
+  data;
+
+  /**
+   * @param {Database} db
+   * @param {IRole} data
+   */
+  constructor(db, data) {
+    this.db = db;
+    this.data = data;
+  }
+
+  /**
+   * Find role by name, and return instance of the Role.
+   * If no role found, return null.
+   *
+   * @param {Database} db - sqlite3 Database instance
+   * @param {string} name - name of the role
+   * @returns {Promise<Role | null>}
+   */
+  static findByName(db, name) {
+    return new Promise((resolve) => {
+      db.get(`SELECT * FROM Roles WHERE name = ?`, [name], function (_, row) {
+        if (!row) resolve(null);
+        resolve(new Role(db, row));
+      });
     });
-  });
-}
+  }
 
-/**
- * Create new role with data.
- * If name is already exists, it will do nothing.
- *
- * @param {Database} db - sqlite3 Database instance
- * @param {Role} data - data that will be inserted into
- * @returns {Promise<void>}
- */
-export function createRole(db, data) {
-  return new Promise((resolve) => {
-    db.run(`INSERT INTO Roles VALUES (?)`, [data.name], resolve);
-  });
-}
+  /**
+   * Create new role with data.
+   * If name is already exists, it will do nothing.
+   * After creation, it will return instance of the Role, or null on error.
+   *
+   * @param {Database} db - sqlite3 Database instance
+   * @param {IRole} data - data that will be inserted into
+   * @returns {Promise<Role | null>}
+   */
+   static create(db, data) {
+     return new Promise((resolve) => {
+       db.run(`INSERT INTO Roles VALUES (?)`, [data.name], function(err) {
+         if (err) resolve(null);
 
-/**
- * Update role data with new data.
- * If name is already exists, it will do nothing.
- *
- * @param {Database} db - sqlite3 Database instance
- * @param {string} name - name of the role
- * @param {Role} data - data that will be updated to
- * @returns {Promise<void>}
- */
-export function updateRole(db, name, data) {
-  return new Promise((resolve) => {
-    const ud = Object.entries(data);
-    if (ud.length === 0) resolve();
+         resolve(new Role(db, data));
+       });
+     });
+   }
 
-    db.run(`UPDATE Roles SET ${ud.map(([k]) => k + '= $' + k).join(', ')} WHERE name = $qname`, {
-      $qname: name,
-      ...Object.fromEntries(ud.map(([k, v]) => ['$' + k, v])),
-    }, resolve);
-  })
-}
+   /**
+    * Update role data with new data.
+    * After successful update, it will update role data.
+    *
+    * @param {Role} data - data that will be updated to
+    * @returns {Promise<void>}
+    */
+  update(data) {
+    return new Promise((resolve) => {
+      const ud = Object.entries(data);
+      if (ud.length === 0) resolve();
 
-/**
- * Delete role found with name.
- * It will do nothing when there's no role found.
- *
- * @param {Database} db - sqlite3 Database instance
- * @param {string} name - name of the role that will be deleted
- * @returns {Promise<void>}
- */
-export function deleteRole(db, name) {
-  return new Promise((resolve) => {
-    db.run(`DELETE FROM Roles WHERE name = ?`, [name], resolve);
-  })
+      this.db.run(`UPDATE Roles SET ${ud.map(([k]) => k + '= $' + k).join(', ')} WHERE name = $qname`, {
+        $qname: this.name,
+        ...Object.fromEntries(ud.map(([k, v]) => ['$' + k, v])),
+      }, function(err) {
+        if (!err) for (const [k,v] of Object.entries(data)) this.data[k] = v;
+        resolve();
+      });
+    })
+  }
+
+  /**
+   * Delete role.
+   */
+   delete() {
+     return new Promise((resolve) => {
+       db.run(`DELETE FROM Roles WHERE name = ?`, [this.data.name], resolve)
+     })
+   }
 }
